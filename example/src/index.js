f6cb81b402f43f9aaaa5c79e083f246db0d52212
Jinho Hong <jinho.hong@huiseoul.com>
2018-07-21 00:20:12 +0900

    sls 형식에 맞게 index.js 변경

--- example/src/index.js
+++ example/src/index.js
@@ -1,21 +1,40 @@
-import { microGraphql, microGraphiql } from "apollo-server-micro";
-import micro, { send } from "micro";
-import { get, post, router } from "microrouter";
+// import { microGraphql, microGraphiql } from "apollo-server-micro";
+// import micro, { send } from "micro";
+// import { get, post, router } from "microrouter";
 
-import schema from "./schema";
+// const graphqlHandler = microGraphql({ schema });
+// const graphiqlHandler = microGraphiql({ endpointURL: "/graphql" });
 
-const graphqlHandler = microGraphql({ schema });
-const graphiqlHandler = microGraphiql({ endpointURL: "/graphql" });
+// const server = micro(
+//   router(
+//     get("/graphql", graphqlHandler),
+//     post("/graphql", graphqlHandler),
+//     get("/graphiql", graphiqlHandler),
+//     (req, res) => send(res, 404, "not found")
+//   )
+// );
 
-const server = micro(
-  router(
-    get("/graphql", graphqlHandler),
-    post("/graphql", graphqlHandler),
-    get("/graphiql", graphiqlHandler),
-    (req, res) => send(res, 404, "not found")
-  )
-);
+// server.listen(3000, () => {
+//   console.log("server is on");
+// });
 
-server.listen(3000, () => {
-  console.log("server is on");
-});
+import "babel-polyfill";
+import { graphqlLambda, graphiqlLambda } from "apollo-server-lambda";
+import lambdaPlayground from "graphql-playground-middleware-lambda";
+
+let schema = "./schema";
+
+if (process.env.NODE_ENV === "sls") {
+  schema = "./" + process.env.NAME + "/index.js";
+}
+
+exports.graphqlHandler = function graphqlHandler(event, context, callback) {
+  function callbackFilter(error, output) {
+    // eslint-disable-next-line no-param-reassign
+    output.headers["Access-Control-Allow-Origin"] = "*";
+    callback(error, output);
+  }
+
+  const handler = graphqlLambda({ schema: schema, tracing: true });
+  return handler(event, context, callbackFilter);
+};
